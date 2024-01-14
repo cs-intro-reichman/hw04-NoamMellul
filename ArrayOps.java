97675de22cfab67db7da6d878be13ae58d9dbc0e
Noam-Mellul <mellul.noam@post.runi.ac.il>
2024-01-14 19:06:34 +0200

    'HW4'

--- ArrayOps.java
+++ ArrayOps.java
@@ -52,13 +52,14 @@ public class ArrayOps {
             sorted(array2);
             set(array1);
             set(array2);
-            if (array1.length != array2.length){
-                return false;
-            }
+
             for ( int i = 0; i < array1.length; i++){
                 if (array1[i] == array2[i]){
                     return true;
                 }
+            }
+            if (array1.length != array2.length){
+                return false;
             }
             return false;
 
